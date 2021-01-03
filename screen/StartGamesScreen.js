dc305a9f69e1274efc796420482b1592dad4e9b2
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-03 07:25:30 +0200

    extract components to reuse them

--- screen/StartGamesScreen.js
+++ screen/StartGamesScreen.js
@@ -1,19 +1,20 @@
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
+import Card from "../components/Card";
 import React from "react";
 
 const StartGamesScreen = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start A New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number!</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -33,14 +34,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: "white",
-    elevation: 8,
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
