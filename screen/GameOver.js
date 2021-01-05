94eac1784d8e186a9695a4e6abc689f042c7158a
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-05 11:42:19 +0200

    fix all oriantation problems

--- screen/GameOver.js
+++ screen/GameOver.js
@@ -1,36 +1,45 @@
 import React from "react";
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+} from "react-native";
 import { Button } from "react-native-elements";
 import colors from "../constants/colors";
 import BodyText from "../components/BodyText";
 
 const GameOver = (props) => {
   return (
-    <View style={styles.screen}>
-      <BodyText style={styles.title}> Game Is Over! </BodyText>
-      <Image
-        source={require("../assets/success.png")}
-        style={styles.image}
-        resizeMode="cover"
-      />
-      <BodyText style={styles.text}>
-        Your phone needed{" "}
-        <Text style={styles.colorfulText}>{props.NumOfRounds}</Text> rounds
-      </BodyText>
-      <BodyText style={styles.text}>
-        to guess the number{" "}
-        <Text style={styles.colorfulText}>{props.realNumber}</Text>.
-      </BodyText>
-      <Button
-        type="clear"
-        titleStyle={{
-          color: colors.primaryColor,
-          fontSize: 20,
-        }}
-        title="NEW GAME"
-        onPress={props.configNewGame}
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <BodyText style={styles.title}> Game Is Over! </BodyText>
+        <Image
+          source={require("../assets/success.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        <BodyText style={styles.text}>
+          Your phone needed{" "}
+          <Text style={styles.colorfulText}>{props.NumOfRounds}</Text> rounds
+        </BodyText>
+        <BodyText style={styles.text}>
+          to guess the number{" "}
+          <Text style={styles.colorfulText}>{props.realNumber}</Text>.
+        </BodyText>
+        <Button
+          type="clear"
+          titleStyle={{
+            color: colors.primaryColor,
+            fontSize: 20,
+          }}
+          title="NEW GAME"
+          onPress={props.configNewGame}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -40,6 +49,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     fontFamily: "open-sans",
+    paddingVertical: 10,
   },
   text: {
     fontWeight: "bold",
